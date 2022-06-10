87818d2145d13c9ccbf8c08be5aa190a7f7aea6f
Hitansh Shah <shah.hitanshsanjay.mat20@itbhu.ac.in>
2022-06-10 20:25:45 +0530

    init commit

---
+++ guide/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@at*b!q74k70i)^#bu@yz@u9_79754+als*%(q_o60s+i+(2b6"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
