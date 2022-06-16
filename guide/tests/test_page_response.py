2aba22b1cdd68f9152184f04e0dc998eaa7a1e15
Hitansh Shah <shah.hitanshsanjay.mat20@itbhu.ac.in>
2022-06-16 20:09:40 +0530

    Add tests to see if pages return 200

---
+++ guide/tests/test_page_response.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from wagtail.models import Page
+from content.models import ContentPage
+from home.models import HomePage
+
+class TestPageResponseStatusCode(TestCase):
+
+    def setUp(self):
+        self.parent = Page.objects.get(url_path='/home/')
+        homePage = HomePage(
+            title = 'Test Home Page',
+            slug = 'test-home-page',
+        )
+        contentPage = ContentPage(
+            title = 'Test Content Page',
+            slug = 'test-content-page',
+            body = [
+                {
+                    'type': 'heading',
+                    'value': "test content test content"
+                }
+            ]
+        )
+        self.parent.add_child(instance=homePage)
+        HomePage.objects.first().add_child(instance=contentPage)
+
+    def test_homepage_response_status_code(self):
+        homepage = HomePage.objects.first()
+        res = self.client.get(homepage.url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_contentpage_response_status_code(self):
+        contentpage = ContentPage.objects.first()
+        res = self.client.get(contentpage.url)
+        self.assertEqual(res.status_code, 200)
