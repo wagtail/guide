3fbfdccc5d5ea0ed1bd854398c1359ae5464c14b
Hitansh Shah <shah.hitanshsanjay.mat20@itbhu.ac.in>
2022-06-17 15:24:20 +0530

    Add fixtures

--- guide/tests/test_page_response.py
+++ guide/tests/test_page_response.py
@@ -4,25 +4,25 @@ from content.models import ContentPage
 from home.models import HomePage
 
 class TestPageResponseStatusCode(TestCase):
-
-    def setUp(self):
-        self.parent = Page.objects.get(url_path='/home/')
-        homePage = HomePage(
-            title = 'Test Home Page',
-            slug = 'test-home-page',
-        )
-        contentPage = ContentPage(
-            title = 'Test Content Page',
-            slug = 'test-content-page',
-            body = [
-                {
-                    'type': 'heading',
-                    'value': "test content test content"
-                }
-            ]
-        )
-        self.parent.add_child(instance=homePage)
-        HomePage.objects.first().add_child(instance=contentPage)
+    fixtures = ["test.json"]
+    # def setUp(self):
+    #     self.parent = Page.objects.get(url_path='/home/')
+    #     homePage = HomePage(
+    #         title = 'Test Home Page',
+    #         slug = 'test-home-page',
+    #     )
+    #     contentPage = ContentPage(
+    #         title = 'Test Content Page',
+    #         slug = 'test-content-page',
+    #         body = [
+    #             {
+    #                 "type": "content",
+    #                 "value": "test content test content"
+    #             }
+    #         ]
+    #     )
+    #     self.parent.add_child(instance=homePage)
+    #     HomePage.objects.first().add_child(instance=contentPage)
 
     def test_homepage_response_status_code(self):
         homepage = HomePage.objects.first()
