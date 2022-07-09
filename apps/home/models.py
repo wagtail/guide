5cc34b6fb826865b6f0a63af6a479157a558067b
Hitansh Shah <shah.hitanshsanjay.mat20@itbhu.ac.in>
2022-07-09 19:46:03 +0530

    Create homepage frontend

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,6 +1,43 @@
+from django.db import models
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 
 
 class HomePage(Page):
     subpage_types = ["content.ContentPage"]
     max_count = 1
+    tutorial_image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+",
+    )
+    how_to_image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+",
+    )
+    explanation_image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+",
+    )
+    reference_image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("tutorial_image"),
+        FieldPanel("how_to_image"),
+        FieldPanel("explanation_image"),
+        FieldPanel("reference_image"),
+    ]
