473434d4ae706b3731e5844d6db35fd4619c9682
Jake Howard <jake.howard@torchbox.com>
2022-08-22 11:16:27 +0100

    Allow running on non-sqlite databases

--- apps/guide/settings/base.py
+++ apps/guide/settings/base.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
@@ -89,11 +90,11 @@ WSGI_APPLICATION = "apps.guide.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600, default=f"sqlite:///{BASE_DIR}/db.sqlite3"
+    )
 }
 
 
