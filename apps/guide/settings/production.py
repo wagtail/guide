dd611ff51c9ca8ae129c2b9215e58a57ebbb9608
Jake Howard <jake.howard@torchbox.com>
2022-08-22 11:25:42 +0100

    Don't import local settings in production

--- apps/guide/settings/production.py
+++ apps/guide/settings/production.py
@@ -7,8 +7,3 @@ SECRET_KEY = env["SECRET_KEY"]  # noqa
 ALLOWED_HOSTS = env["ALLOWED_HOSTS"].split(",")  # noqa
 
 MANIFEST_LOADER["cache"] = True  # noqa
-
-try:
-    from .local import *  # noqa
-except ImportError:
-    pass
