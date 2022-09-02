7fe34d25d4217675d0d26d6d8a6e45f21ad67823
Coen van der Kamp <coen@fourdigits.nl>
2022-09-02 14:00:05 +0200

    Add table of contents and title perm links (#31)

---
+++ apps/frontend/static_src/js/section-link/index.js
@@ -0,0 +1,14 @@
+export const initSectionLink = () => {
+  const headings = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+  headings.forEach((heading) => {
+    if (heading.id) {
+      let sectionLink = document.createElement("a");
+      let link = document.createTextNode("¶");
+      sectionLink.appendChild(link);
+      sectionLink.setAttribute("href", "#" + heading.id);
+      sectionLink.setAttribute("class", "section-link");
+      sectionLink.setAttribute("title", "Section link");
+      heading.appendChild(sectionLink);
+    }
+  });
+}
