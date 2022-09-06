b734d01f6e56d43fed4a04d63baf363155af2f86
Hitansh Shah <76250591+Hitansh-Shah@users.noreply.github.com>
2022-09-06 20:18:41 +0530

    refactor previous and next buttons logic (#61)

--- apps/core/models/content.py
+++ apps/core/models/content.py
@@ -43,3 +43,13 @@ class ContentPage(Page):
 
     content_panels = Page.content_panels + [FieldPanel("body")]
     base_form_class = ContentPageForm
+
+    def get_context(self, request, *args, **kwargs):
+        if self.live and self.show_in_menus:
+            context = super().get_context(request, *args, **kwargs)
+            pages = Page.objects.live().in_menu()
+            context.update(
+                previous=pages.filter(path__lt=self.path).last(),
+                next=pages.filter(path__gt=self.path).first(),
+            )
+            return context
