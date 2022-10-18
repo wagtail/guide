57760d8d6b3b13c7a5b7e3bd2914211cae80fd98
Nick Lee <nick@nicklee.co.uk>
2022-10-18 13:35:55 +0100

    adding test pages for 404/500

--- apps/guide/urls.py
+++ apps/guide/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.contrib.sitemaps.views import sitemap
@@ -28,6 +29,18 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += [
+        # Add views for testing 404 and 500 templates
+        path(
+            "test404/",
+            TemplateView.as_view(template_name="404.html"),
+        ),
+        path(
+            "test500/",
+            TemplateView.as_view(template_name="500.html"),
+        ),
+    ]
 
 urlpatterns += i18n_patterns(
     path("", include(wagtail_urls)),
