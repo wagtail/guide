a6d5fcca78a851bbdf11c794c5943d10f8da124f
LB Johnston <mail@lb.ee>
2022-10-20 07:05:12 +1000

    Eslint fixes
    - Update variable & function names to align with Eslint
    - Update cookies function to avoid using loop and instead use functional style
    - Reorder main.js functions to avoid using hoisting / globals
    - Remove unused variables/imports
    - Adds some JSDOC syntax for better code documentation
    - Fixes #112
    - Relates to #151

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
