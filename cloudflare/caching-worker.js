5f193cb6bd389b3a972e3ab78456ccf8c2a4019c
Jake Howard <jake.howard@torchbox.com>
2022-10-20 14:36:44 +0100

    Run prettier over worker

--- cloudflare/caching-worker.js
+++ cloudflare/caching-worker.js
@@ -3,25 +3,25 @@
 // applied after this worker runs.
 
 // When any cookie in this list is present in the request, cache will be skipped
-const PRIVATE_COOKIES = ["csrftoken", "sessionid"];
+const PRIVATE_COOKIES = ['csrftoken', 'sessionid'];
 
 // These querystring keys are stripped from the request as they are generally not
 // needed by the origin.
 const STRIP_QUERYSTRING_KEYS = [
-    "utm_source",
-    "utm_campaign",
-    "utm_medium",
-    "utm_term",
-    "utm_content",
-    "gclid",
-    "fbclid",
-    "dm_i", // DotDigital
-    "msclkid",
+    'utm_source',
+    'utm_campaign',
+    'utm_medium',
+    'utm_term',
+    'utm_content',
+    'gclid',
+    'fbclid',
+    'dm_i', // DotDigital
+    'msclkid',
 ];
 
 // If this is true, the querystring keys stripped from the request will be
 // addeed to any Location header served by a redirect.
-const REPLACE_STRIPPED_QUERYSTRING_ON_REDIRECT_LOCATION = false
+const REPLACE_STRIPPED_QUERYSTRING_ON_REDIRECT_LOCATION = false;
 
 // If this is true, querystring key are stripped if they have no value eg. ?foo
 // Disabled by default, but highly recommended
@@ -31,7 +31,7 @@ const STRIP_VALUELESS_QUERYSTRING_KEYS = false;
 // (from https://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html#sec13.4)
 const CACHABLE_HTTP_STATUS_CODES = [200, 203, 206, 300, 301, 410];
 
-addEventListener("fetch", (event) => {
+addEventListener('fetch', (event) => {
     event.respondWith(main(event));
 });
 
@@ -58,7 +58,10 @@ async function main(event) {
     }
 
     if (REPLACE_STRIPPED_QUERYSTRING_ON_REDIRECT_LOCATION) {
-        response = replaceStrippedQsOnRedirectResponse(response, strippedParams);
+        response = replaceStrippedQsOnRedirectResponse(
+            response,
+            strippedParams,
+        );
     }
 
     return response;
@@ -72,7 +75,7 @@ function requestIsCachable(request) {
      * Given a Request, determine if it should be cached.
      * Currently the only factor here is whether a private cookie is present.
      */
-    return !hasPrivateCookie(request)
+    return !hasPrivateCookie(request);
 }
 
 function responseIsCachable(response) {
@@ -80,9 +83,8 @@ function responseIsCachable(response) {
      * Given a Response, determine if it should be cached.
      * Currently the only factor here is whether the status code is cachable.
      */
-    return CACHABLE_HTTP_STATUS_CODES.includes(response.status)
+    return CACHABLE_HTTP_STATUS_CODES.includes(response.status);
 }
-
 
 /*
  * Request Utilities
@@ -125,17 +127,17 @@ function hasPrivateCookie(request) {
     /*
      * Given a Request, determine if one of the 'private' cookies are present.
      */
-    const cookieHeader = request.headers.get("Cookie");
+    const cookieHeader = request.headers.get('Cookie');
     if (!cookieHeader) {
         return false;
     }
 
     const requestCookieNames = cookieHeader
-        .split(";")
-        .map((cookie) => cookie.split("=")[0].trim());
+        .split(';')
+        .map((cookie) => cookie.split('=')[0].trim());
 
     return PRIVATE_COOKIES.some((privateCookieName) =>
-        requestCookieNames.includes(privateCookieName)
+        requestCookieNames.includes(privateCookieName),
     );
 }
 
@@ -153,7 +155,7 @@ function replaceStrippedQsOnRedirectResponse(response, strippedParams) {
     response = new Response(response.body, response);
 
     if ([301, 302].includes(response.status)) {
-        const locationHeaderValue = response.headers.get("location");
+        const locationHeaderValue = response.headers.get('location');
         let locationUrl;
 
         if (!locationHeaderValue) {
@@ -165,9 +167,12 @@ function replaceStrippedQsOnRedirectResponse(response, strippedParams) {
         if (!isAbsolute) {
             // If the Location URL isn't absolute, we need to provide a Host so we can use
             // a URL object.
-            locationUrl = new URL(locationHeaderValue, "http://www.example.com");
+            locationUrl = new URL(
+                locationHeaderValue,
+                'http://www.example.com',
+            );
         } else {
-            locationUrl = new URL(locationHeaderValue)
+            locationUrl = new URL(locationHeaderValue);
         }
 
         for (const [key, value] of Object.entries(strippedParams)) {
@@ -177,20 +182,20 @@ function replaceStrippedQsOnRedirectResponse(response, strippedParams) {
         let newLocation;
 
         if (isAbsolute) {
-            newLocation = locationUrl.toString()
+            newLocation = locationUrl.toString();
         } else {
             newLocation = `${locationUrl.pathname}${locationUrl.search}`;
         }
 
-        response.headers.set("location", newLocation);
+        response.headers.set('location', newLocation);
     }
 
-    return response
+    return response;
 }
 
 /**
  * URL Utilities
  */
 function isUrlAbsolute(url) {
-    return (url.indexOf('://') > 0 || url.indexOf('//') === 0);
+    return url.indexOf('://') > 0 || url.indexOf('//') === 0;
 }
