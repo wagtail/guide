8689a4ec7aacc8bef1034c6a91e3cad9a30ff24c
Sage Abdullah <sage.abdullah@torchbox.com>
2022-10-20 16:48:04 +0100

    Create get_version_from_language_code template tag to better handle languages with regional codes

--- apps/core/templatetags/core_tags.py
+++ apps/core/templatetags/core_tags.py
@@ -25,3 +25,8 @@ def navigation(context):
 @register.filter
 def hreflang_url(value, arg):
     return value.replace(f"/{arg}/", "/en-latest/", 1)
+
+
+@register.simple_tag
+def get_version_from_language_code(language_code):
+    return language_code.rsplit("-", maxsplit=1)[1]
