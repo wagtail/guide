b949ed70cf1f0135815f50953907398065bc1b1c
sag᠎e <sage.abdullah@torchbox.com>
2022-10-24 19:00:06 +0100

    Fix monkey patch for `get_language_info()` (#178)
    * Add test to ensure account settings can be accessed
    
    * Fix patch for get_language_info

--- apps/core/tests/test_locales.py
+++ apps/core/tests/test_locales.py
@@ -1,7 +1,9 @@
 from http import HTTPStatus
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 from apps.core.factories import HomePageFactory, LocaleFactory
 
@@ -13,6 +15,9 @@ class TestPageLocales(TestCase):
         self.nl = LocaleFactory(language_code="nl-4.1.x")
         self.home_page_nl = self.home_page_en.copy_for_translation(self.nl)
         self.home_page_nl.save_revision().publish()
+        self.user = get_user_model().objects.create_superuser(
+            username="admin", password="test"
+        )
 
     def test_language_code_in_settings_languages(self):
         language_codes = [key for key, _ in settings.LANGUAGES]
@@ -26,3 +31,9 @@ class TestPageLocales(TestCase):
         self.assertEqual(self.home_page_nl.url, "/nl-4.1.x/")
         response = self.client.get(self.home_page_nl.url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_can_access_account_settings(self):
+        self.client.login(username="admin", password="test")
+        response = self.client.get(reverse("wagtailadmin_account"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, "简体中文")
--- monkey_patches.py
+++ monkey_patches.py
@@ -11,6 +11,7 @@ Valid examples:
 import re
 import logging
 
+from django.conf import settings
 from django.utils import translation
 from django.utils.translation import get_language_info as original_get_language_info
 from django.utils.translation import gettext_lazy, trans_real
@@ -31,37 +32,17 @@ trans_real.language_code_prefix_re = _lazy_re_compile(
 )
 
 
+def remove_version_number_from_language_code(lang_code):
+    # Make sure to remove the version only if it exists
+    if any((version in lang_code for version in settings.WAGTAIL_GUIDE_VERSIONS)):
+        return lang_code.rsplit("-", maxsplit=1)[0]
+    return lang_code
+
+
 def patched_get_language_info(lang_code):
-    from django.conf.locale import LANG_INFO
-
     # PATCH: remove version number before continuing with the original function
-    lang_code = lang_code.rsplit("-", maxsplit=1)[0]
-
-    # Need to copy the original code instead of calling the original function
-    # directly because there's a recursive call to `get_language_info` in the
-    # original function.
-
-    try:
-        lang_info = LANG_INFO[lang_code]
-        if "fallback" in lang_info and "name" not in lang_info:
-            # PATCH: recurse with original function
-            info = original_get_language_info(lang_info["fallback"][0])
-        else:
-            info = lang_info
-    except KeyError:
-        if "-" not in lang_code:
-            raise KeyError("Unknown language code %s." % lang_code)
-        generic_lang_code = lang_code.split("-")[0]
-        try:
-            info = LANG_INFO[generic_lang_code]
-        except KeyError:
-            raise KeyError(
-                "Unknown language code %s and %s." % (lang_code, generic_lang_code)
-            )
-
-    if info:
-        info["name_translated"] = gettext_lazy(info["name"])
-    return info
+    lang_code = remove_version_number_from_language_code(lang_code)
+    return original_get_language_info(lang_code)
 
 
 translation.get_language_info = patched_get_language_info
