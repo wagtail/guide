6b95fa9a4b951c8d059c32b969955922e252a76f
Sage Abdullah <sage.abdullah@torchbox.com>
2022-10-25 17:23:07 +0100

    Properly generate hreflang links to translations of pages

--- apps/core/tests/test_alternate_link_tag.py
+++ apps/core/tests/test_alternate_link_tag.py
@@ -1,38 +1,83 @@
-import json
-
 from django.test import TestCase
 
 from apps.core.factories import ContentPageFactory, HomePageFactory, LocaleFactory
 
 
-class TestAlternateLinkTag(TestCase):
+class TestAlternateLinkTagsHomePage(TestCase):
     def setUp(self):
-        self.en = LocaleFactory(language_code="en-latest")
-        self.home_page_en = HomePageFactory(locale=self.en)
-        self.nl = LocaleFactory(language_code="nl-4.1.x")
-        self.home_page_nl = self.home_page_en.copy_for_translation(self.nl)
-        self.home_page_nl.save_revision().publish()
-        body = json.dumps(
-            [
-                {"type": "text", "value": "<p>some test content</p>"},
-            ],
+        self.en_latest = LocaleFactory(language_code="en-latest")
+        self.en_41x = LocaleFactory(language_code="en-4.1.x")
+        self.nl_41x = LocaleFactory(language_code="nl-4.1.x")
+        self.is_41x = LocaleFactory(language_code="is-4.1.x")
+
+        self.page_en_latest = HomePageFactory(locale=self.en_latest)
+        self.page_en_41x = self.page_en_latest.copy_for_translation(self.en_41x)
+        self.page_en_41x.save_revision().publish()
+        self.page_nl_41x = self.page_en_latest.copy_for_translation(self.nl_41x)
+        self.page_nl_41x.save_revision().publish()
+        self.page_is_41x = self.page_en_latest.copy_for_translation(self.is_41x)
+        self.page_is_41x.save_revision().publish()
+
+    def test_en_latest_no_alternate_tags(self):
+        response = self.client.get(self.page_en_latest.full_url)
+        # Should not have the alternate tags
+        # as there are no translations for the latest version
+        self.assertNotContains(response, '<link rel="alternate"')
+
+    def test_en_41x_alternate_tags(self):
+        response = self.client.get(self.page_en_41x.full_url)
+        # Should have the alternate tag for the
+        # Dutch 4.1.x and Icelandic 4.1.x versions only
+        href = self.page_nl_41x.full_url
+        self.assertContains(
+            response,
+            f'<link rel="alternate" hreflang="nl" href="{href}">',
         )
-        self.content_page_en = ContentPageFactory(body=body, locale=self.en)
-        self.content_page_nl = self.content_page_en.copy_for_translation(self.nl)
-        self.content_page_nl.save_revision().publish()
+        href = self.page_is_41x.full_url
+        self.assertContains(
+            response,
+            f'<link rel="alternate" hreflang="is" href="{href}">',
+        )
 
-    def test_home_page_nl_has_alternate_tag(self):
-        response = self.client.get(self.home_page_nl.url)
-        self.assertContains(response, '<link rel="alternate" hreflang="en"')
+        # Should not have the alternate tag for the English latest version
+        href = self.page_en_latest.full_url
+        self.assertNotContains(
+            response,
+            f'<link rel="alternate" hreflang="en" href="{href}">',
+        )
+        self.assertContains(response, '<link rel="alternate"', count=2)
 
-    def test_content_page_nl_has_alternate_tag(self):
-        response = self.client.get(self.content_page_nl.url)
-        self.assertContains(response, '<link rel="alternate" hreflang="en"')
+    def test_nl_41x_alternate_tags(self):
+        response = self.client.get(self.page_nl_41x.full_url)
+        # Should have the alternate tag for the
+        # English 4.1.x and Icelandic 4.1.x versions only
+        href = self.page_en_41x.full_url
+        self.assertContains(
+            response,
+            f'<link rel="alternate" hreflang="en" href="{href}">',
+        )
+        href = self.page_is_41x.full_url
+        self.assertContains(
+            response,
+            f'<link rel="alternate" hreflang="is" href="{href}">',
+        )
 
-    def test_home_page_en_has_no_alternate_tag(self):
-        response = self.client.get(self.home_page_en.url)
-        self.assertNotContains(response, '<link rel="alternate" hreflang="en"')
+        # Should not have the alternate tag for the English latest version
+        href = self.page_en_latest.full_url
+        self.assertNotContains(
+            response,
+            f'<link rel="alternate" hreflang="en" href="{href}">',
+        )
+        self.assertContains(response, '<link rel="alternate"', count=2)
 
-    def test_content_page_en_has_no_alternate_tag(self):
-        response = self.client.get(self.content_page_en.url)
-        self.assertNotContains(response, '<link rel="alternate" hreflang="en"')
+
+class TestAlternateLinkTagsContentPage(TestAlternateLinkTagsHomePage):
+    def setUp(self):
+        super().setUp()
+        self.page_en_latest = ContentPageFactory(locale=self.en_latest)
+        self.page_en_41x = self.page_en_latest.copy_for_translation(self.en_41x)
+        self.page_en_41x.save_revision().publish()
+        self.page_nl_41x = self.page_en_latest.copy_for_translation(self.nl_41x)
+        self.page_nl_41x.save_revision().publish()
+        self.page_is_41x = self.page_en_latest.copy_for_translation(self.is_41x)
+        self.page_is_41x.save_revision().publish()
