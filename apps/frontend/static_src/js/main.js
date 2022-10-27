ae562180377d1a565ed3583e86f56c28d8e7d2de
Sævar Öfjörð Magnússon <saevar@saevar.is>
2022-10-27 15:50:08 +0000

    Linting

--- apps/frontend/static_src/js/main.js
+++ apps/frontend/static_src/js/main.js
@@ -54,7 +54,9 @@ const onSearchInputChange = async (event) => {
     const query = event.target.value;
     try {
         const res = await fetch(
-            `${window.location.origin}${window.languageCode ? `/${window.languageCode}` : '' }/search_json/?${new URLSearchParams({
+            `${window.location.origin}${
+                window.languageCode ? `/${window.languageCode}` : ''
+            }/search_json/?${new URLSearchParams({
                 query,
             }).toString()}`,
         );
