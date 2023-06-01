b4ca3be8c6d21185e9e0ec7e2ab7537e0c4ad27b
Sage Abdullah <sage.abdullah@torchbox.com>
2023-06-01 16:48:37 +0100

    Add test to update happy feedback with text

--- apps/core/tests/test_feedback.py
+++ apps/core/tests/test_feedback.py
@@ -42,7 +42,7 @@ class TestFeedback(TestCase):
         self.assertEqual(feedback_obj.feedback_text, "")
         self.assertEqual(feedback_obj.page.specific, self.target_page)
 
-    def test_update_feedback(self):
+    def test_update_feedback_unhappy(self):
         payload1 = {
             "feedback": "unhappy",
         }
@@ -52,7 +52,7 @@ class TestFeedback(TestCase):
             content_type="application/json; charset=UTF-8",
         )
         data1 = json.loads(res1.content)
-        payload2 = {"pk": data1["pk"], "feedback_text": "some test feedback text"}
+        payload2 = {"pk": data1["pk"], "feedback_text": "some unhappy feedback text"}
         res2 = self.client.post(
             self.target_page.url,
             json.dumps(payload2),
@@ -61,8 +61,31 @@ class TestFeedback(TestCase):
         data2 = json.loads(res2.content)
         self.assertEqual(data1["pk"], data2["pk"])
         feedback_obj = Feedback.objects.get(pk=data1["pk"])
-        self.assertEqual(feedback_obj.feedback_text, "some test feedback text")
+        self.assertEqual(feedback_obj.feedback_text, "some unhappy feedback text")
         self.assertEqual(feedback_obj.feedback, "unhappy")
+        self.assertEqual(feedback_obj.page.specific, self.target_page)
+
+    def test_update_feedback_happy(self):
+        payload1 = {
+            "feedback": "happy",
+        }
+        res1 = self.client.post(
+            self.target_page.url,
+            json.dumps(payload1),
+            content_type="application/json; charset=UTF-8",
+        )
+        data1 = json.loads(res1.content)
+        payload2 = {"pk": data1["pk"], "feedback_text": "some happy feedback text"}
+        res2 = self.client.post(
+            self.target_page.url,
+            json.dumps(payload2),
+            content_type="application/json; charset=UTF-8",
+        )
+        data2 = json.loads(res2.content)
+        self.assertEqual(data1["pk"], data2["pk"])
+        feedback_obj = Feedback.objects.get(pk=data1["pk"])
+        self.assertEqual(feedback_obj.feedback_text, "some happy feedback text")
+        self.assertEqual(feedback_obj.feedback, "happy")
         self.assertEqual(feedback_obj.page.specific, self.target_page)
 
     def test_csrf_token_exists(self):
