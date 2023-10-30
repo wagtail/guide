2ba3153b39e895e1ba7329a1a291a570e4dd4ea7
Tidiane <atdia97@gmail.com>
2023-10-30 13:09:56 +0000

    Monkey patch CopyPageForTranslationAction._copy_for_translation after apps are installed

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.core"
+
+    def ready(self):
+        from .monkey_patches import patch_copy_for_translation_action
+
+        patch_copy_for_translation_action()
