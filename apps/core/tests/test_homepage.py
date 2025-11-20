8820e331f1f69e55069995b16f6f0ca1562fbabe
Ernesto Rico Schmidt <ernesto@rico-schmidt.name>
2025-11-20 16:38:10 -0400

    Format

--- apps/core/tests/test_homepage.py
+++ apps/core/tests/test_homepage.py
@@ -6,12 +6,14 @@ from apps.core.factories import HomePageFactory
 class TestHomePage(TestCase):
     def setUp(self):
         self.homepage = HomePageFactory()
-        self.homepage.introduction = ('<p><i>Want to learn more about Wagtail’s '
-                                      'future?</i></p>')
+        self.homepage.introduction = (
+            "<p><i>Want to learn more about Wagtail’s future?</i></p>"
+        )
         self.homepage.save()
 
     def test_render_homepage_with_introduction(self):
         response = self.client.get(self.homepage.url)
 
-        self.assertContains(response, ('<p><i>Want to learn more about Wagtail’s '
-                                      'future?</i></p>'))
+        self.assertContains(
+            response, ("<p><i>Want to learn more about Wagtail’s future?</i></p>")
+        )
