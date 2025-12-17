7815995c425c8d85d6ee2b05ee32a5c9b3f20810
Thibaud Colas <thibaudcolas@gmail.com>
2025-12-17 11:37:53 +0000

    Clean up llms.txt files implementation

--- apps/llms_txt/views.py
+++ apps/llms_txt/views.py
@@ -1,27 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.cache import cache_control
 from wagtail.contrib.sitemaps import Sitemap
 
 
+@cache_control(max_age=3600)
 def llms_txt_view(request):
-    pages = Sitemap().items()
+    pages = Sitemap(request).items()
     template = loader.get_template("llms_txt/llms.txt.jinja")
-    context = {
-        "pages": pages,
-        "request": request,
-    }
+    context = {"pages": pages}
     content = template.render(context, request)
     response = HttpResponse(content, content_type="text/plain; charset=utf-8")
     return response
 
 
+@cache_control(max_age=3600)
 def llms_full_txt_view(request):
-    pages = Sitemap().items()
+    pages = Sitemap(request).items()
     template = loader.get_template("llms_txt/llms-full.txt.jinja")
-    context = {
-        "pages": pages,
-        "request": request,
-    }
+    context = {"pages": pages}
     content = template.render(context, request)
     response = HttpResponse(content, content_type="text/plain; charset=utf-8")
     return response
