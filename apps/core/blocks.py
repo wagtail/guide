2cebbbac18b49f23ab451f1bba3c5dc2af0ac5dc
Raghad Dahi <raghaddahi27@gmail.com>
2026-06-19 16:34:22 +0300

    Add VersionNoteBlock for inline version annotations in content pages

--- apps/core/blocks.py
+++ apps/core/blocks.py
@@ -2,6 +2,25 @@ from django.utils.translation import gettext as _
 from wagtail import blocks
 from wagtail.blocks import RichTextBlock
 
+WAGTAIL_VERSIONS = [
+    "4.1",
+    "4.2",
+    "5.0",
+    "5.1",
+    "5.2",
+    "6.0",
+    "6.1",
+    "6.2",
+    "6.3",
+    "6.4",
+    "7.0",
+    "7.1",
+    "7.2",
+    "7.3",
+    "7.4",
+    "8.0",
+]
+
 
 class TextBlock(RichTextBlock):
     class Meta:
@@ -62,5 +81,48 @@ class AlertBlock(blocks.StructBlock):
         value_class = AlertStructValue
 
 
-CONTENT_BLOCKS = [("text", TextBlock()), ("alert", AlertBlock())]
+class VersionNoteStructValue(blocks.StructValue):
+    def icon(self):
+        return f"core/svg/{self.get('change_type')}.svg"
+
+    def change_type_display(self):
+        change_type = self.get("change_type")
+        choices = {
+            "added": _("Added"),
+            "changed": _("Changed"),
+            "removed": _("Removed"),
+        }
+        return choices.get(change_type, change_type)
+
+
+class VersionNoteBlock(blocks.StructBlock):
+    version = blocks.ChoiceBlock(
+        choices=[(v, v) for v in WAGTAIL_VERSIONS],
+        label=_("Version"),
+    )
+    change_type = blocks.ChoiceBlock(
+        choices=[
+            ("added", _("Added")),
+            ("changed", _("Changed")),
+            ("removed", _("Removed")),
+        ],
+        label=_("Type of change"),
+    )
+    content = RichTextBlock(
+        features=["bold", "italic", "link"],
+        label=_("Content"),
+    )
+
+    class Meta:
+        template = "core/blocks/version_note.html"
+        icon = "tag"
+        label = _("Version note")
+        value_class = VersionNoteStructValue
+
+
+CONTENT_BLOCKS = [
+    ("text", TextBlock()),
+    ("alert", AlertBlock()),
+    ("version_note", VersionNoteBlock()),
+]
 HOME_BLOCKS = [("section_grid", SectionGridBlock())]
